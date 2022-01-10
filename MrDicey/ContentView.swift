6beb0adff1c788f121ef97e33a0848ae7c3507cb
lando2319 <mikepland@gmail.com>
2022-01-10 16:57:43 -0600

    Initial Commit

---
+++ MrDicey/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MrDicey
+//
+//  Created by MIKE LAND on 1/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MrDicey/MrDiceyApp.swift
@@ -0,0 +1,17 @@
+//
+//  MrDiceyApp.swift
+//  MrDicey
+//
+//  Created by MIKE LAND on 1/10/22.
+//
+
+import SwiftUI
+
+@main
+struct MrDiceyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
