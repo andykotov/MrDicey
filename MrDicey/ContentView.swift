0e8984e1d95bca62935f6bf9266d439fc5c99f0f
lando2319 <mikepland@gmail.com>
2022-01-10 17:42:26 -0600

    adds assets for diceRoll5-6, initial setup for 'old' dice

--- MrDicey/ContentView.swift
+++ MrDicey/ContentView.swift
@@ -8,9 +8,36 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var diceActual = Image("diceRoll5-6")
+    @State private var dragOffset = CGSize.zero
+
     var body: some View {
-        Text("Mr. Dicey")
-            .padding()
+        GeometryReader { metrics in
+            VStack(content: {
+                Text("Mr. Dicey")
+                    .padding()
+                    .frame(width: metrics.size.width)
+                
+                Spacer()
+                
+                diceActual
+                    .resizable()
+                    .scaledToFit()
+                    .offset(dragOffset)
+                    .gesture(
+                        DragGesture()
+                            .onChanged { gesture in
+                                dragOffset = gesture.translation
+                            }
+                            .onEnded { gesture in
+                                dragOffset = .zero
+                            }
+                    )
+                    .frame(width: metrics.size.width * 0.25)
+
+                Spacer()
+            })
+        }
     }
 }
 
