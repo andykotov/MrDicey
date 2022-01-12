6c22a040835ed849525d092dfcd1639d62f1582b
IHakoda <Hakoda.AK@gmail.com>
2022-01-12 21:53:49 +0200

    Tried SceneKit
    Tried setting up a scene from SceneKit

--- MrDicey/ContentView.swift
+++ MrDicey/ContentView.swift
@@ -6,39 +6,44 @@
 //
 
 import SwiftUI
+import SceneKit
 
 struct ContentView: View {
-    @State var diceActual = Image("diceRoll5-6")
-    @State private var dragOffset = CGSize.zero
+    static func makeScene() -> SCNScene? {
+      let scene = SCNScene(named: "MainScene.scn")
+      return scene
+    }
+    var scene = makeScene()
 
     var body: some View {
-        GeometryReader { metrics in
-            VStack(content: {
+        GeometryReader { geo in
+            VStack{
                 Text("Mr. Dicey")
                     .padding()
-                    .frame(width: metrics.size.width)
+                    .frame(width: geo.size.width)
                 
                 Spacer()
                 
-                diceActual
-                    .resizable()
-                    .scaledToFit()
-                    .offset(dragOffset)
-                    .gesture(
-                        DragGesture()
-                            .onChanged { gesture in
-                                dragOffset = gesture.translation
-                            }
-                            .onEnded { gesture in
-                                dragOffset = .zero
-                            }
-                    )
-                    .frame(width: metrics.size.width * 0.25)
-
-                Spacer()
-            })
+                SceneView(
+                  // 1
+                  scene: scene,
+                  // 2
+                  pointOfView: setUpCamera(),
+                  // 3
+                  options: []
+                )
+                .background(Color.secondary)
+                .edgesIgnoringSafeArea(.all)
+            }
         }
     }
+    
+    func setUpCamera() -> SCNNode? {
+      let cameraNode = scene?.rootNode
+        .childNode(withName: "camera", recursively: false)
+      return cameraNode
+    }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
